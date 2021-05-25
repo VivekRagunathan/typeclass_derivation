ea8e2929025083bbccdab353ea9cf68fba5d7ecf
Vivek Ragunathan <vragunathan@livongo.com>
2021-05-24 22:25:29 -0700

    Minor updates

--- src/main/scala/tcd/Maybe.scala
+++ src/main/scala/tcd/Maybe.scala
@@ -5,9 +5,13 @@ enum Opt[+T] derives Eq:
   case Nn
 
 object Main:
+  import Opt.*
+
   def main(args: Array[String]): Unit =
-    import Opt.*
+    // NOTE: Get the following error on this line
+    //    cannot reduce summonFrom with
+    //      patterns :  case given t @ _:tcd.Eq[tcd.Opt.Sm[Int]]
     val eqoi = summon[Eq[Opt[Int]]]
-    // assert(eqoi.eqv(Sm(23), Sm(23)))
-    // assert(!eqoi.eqv(Sm(23), Sm(13)))
-    // assert(!eqoi.eqv(Sm(23), Nn))
+    assert(eqoi.eqv(Sm(23), Sm(23)))
+    assert(!eqoi.eqv(Sm(23), Sm(13)))
+    assert(!eqoi.eqv(Sm(23), Nn))
